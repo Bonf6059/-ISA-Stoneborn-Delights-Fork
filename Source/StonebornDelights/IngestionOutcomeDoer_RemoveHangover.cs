1b248b46dbb55fcc66892c427128979881732225
Bonf6059 <87819153+Bonf6059@users.noreply.github.com>
2025-11-24 07:43:39 +0100

    Add files via upload

---
+++ Source/StonebornDelights/IngestionOutcomeDoer_RemoveHangover.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace StonebornDelights
+{
+	public class IngestionOutcomeDoer_RemoveHangover : IngestionOutcomeDoer
+	{
+		public override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+			Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(SD_DefOf.SD_LightHangover);
+			if (firstHediffOfDef != null)
+			{
+				pawn.health.RemoveHediff(firstHediffOfDef);
+			}
+		}
+    }
+}
